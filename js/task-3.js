ab73e5b9af3a585e40c2cbe96fbf2ae839d9a51e
yurakh-maxym-dfg <maksaaasssdfg@gmail.com>
2026-03-15 18:13:58 +0200

    task-3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+
+  changeUsername(newName){
+    for (const name of this.username){
+        this.username = newName;
+    } 
+    
+  },
+
+  updatePlayTime(hours){
+        this.playTime += hours;
+  },
+  getInfo(){
+    return `${this.username} has ${this.playTime} cative hours!`
+  },
+};
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
